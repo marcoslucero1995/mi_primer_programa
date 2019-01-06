0e3fe8b178123b3bf1b472504c17e00113852de1
Lucero <marcoslucero1995@gmail.com>
2019-01-06 19:31:52 -0300

    Añadiendo el archivo nuevo

--- Adivina el numero.py
+++ Adivina el numero.py
@@ -1,9 +1,36 @@
 
-numero_a_adivinar = 2
+numero_a_adivinar = 9
 
 user_number = int(input("adivina un numero: "))
 
 if user_number == numero_a_adivinar:
     print("has ganado hostias")
 else:
-    print("has perdido")
+    print("segundo intento")
+user_number = int(input("adivina un numero: "))
+
+if user_number == numero_a_adivinar:
+    print("has ganado hostias")
+else:
+    print("tercer intento")
+
+user_number = int(input("adivina un numero: "))
+if user_number == numero_a_adivinar:
+    print("has ganado hostias")
+else:
+    print("cuarto intento andale ya")
+
+user_number = int(input("adivina un numero: "))
+
+if user_number == numero_a_adivinar:
+    print("has ganado hostias")
+
+else:
+    print("quinto y ultimo intento")
+
+user_number = int(input("adivina un numero ultimo intento"))
+
+if user_number == numero_a_adivinar:
+    print("al fin has ganado costo")
+else:
+    print("no has podido jajaja")
