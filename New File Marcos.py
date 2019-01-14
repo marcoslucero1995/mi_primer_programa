f8f218e7b7618a1d87bda63c99c96445af91f8bd
Lucero <marcoslucero1995@gmail.com>
2019-01-13 21:57:59 -0300

    Añadiendo el archivo nuevo

--- New File Marcos.py
+++ New File Marcos.py
@@ -1,36 +1,40 @@
 
+primer_numero = 0
+segundo_numero = 0
 
 operacion_a_realizar =  input("¿Que operacion deseas realizar? ( + : - : / : *: ")
 
-while operacion_a_realizar == "+" or "-" or "/" or "*":
+operacion_a_realizar == "+" or "-" or "/" or "*"
 
-        primer_numero = float(input("Ingresa primer numero: "))
+primer_numero = float(input("Ingresa primer numero: "))
 
-        segundo_numero = float(input("Ingresa segundo numero: "))
+segundo_numero = float(input("Ingresa segundo numero: "))
 
-        suma = primer_numero + segundo_numero
+primer_numero == float
 
-        resta = primer_numero - segundo_numero
+suma = primer_numero + segundo_numero
 
-        multiplicacion = primer_numero * segundo_numero
+resta = primer_numero - segundo_numero
 
-        division = primer_numero / segundo_numero
+multiplicacion = primer_numero * segundo_numero
 
-        resultado = 0
+division = primer_numero / segundo_numero
 
-        if operacion_a_realizar == "+":
-            resultado = suma
-            print("El resultado de la operacion es {}".format(resultado))
+resultado = 0
 
-        elif operacion_a_realizar == "-":
-            resultado = resta
-            print("El resultado de la operacion es {}".format(resultado))
+if operacion_a_realizar == "+":
+    resultado = suma
+    print("El resultado de la operacion es {}".format(resultado))
 
-        elif operacion_a_realizar == "/":
-            resultado = division
-            print("El resultado de la operacion es {}".format(resultado))
+elif operacion_a_realizar == "-":
+    resultado = resta
+    print("El resultado de la operacion es {}".format(resultado))
 
-        elif operacion_a_realizar == "*":
-            resultado = multiplicacion
-            print("El resultado de la operacion es {}".format(resultado))
+elif operacion_a_realizar == "/":
+    resultado = division
+    print("El resultado de la operacion es {}".format(resultado))
+
+elif operacion_a_realizar == "*":
+    resultado = multiplicacion
+    print("El resultado de la operacion es {}".format(resultado))
 
